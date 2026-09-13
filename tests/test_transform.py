2278084a0440a9bec496b479464595d54dceac7b
Night-SHANG <59331024+Night-SHANG@users.noreply.github.com>
2026-09-13 23:12:08 +0800

    test: reproduce WinUI Window localization crash

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -10,6 +10,15 @@ def test_xaml_localizes_literals_and_tooltips():
     assert entries["B.ToolTipService.ToolTip"] == "Reload data"
 
 
+def test_xaml_does_not_attach_localizer_uid_to_winui_window():
+    src = '<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml" Title="RHI"><TextBlock Text="Hello"/></Window>'
+    out, entries = transform_xaml(src, "MainWindow.xaml")
+    window_tag = out.split(">", 1)[0]
+    assert 'l:Uids.Uid=' not in window_tag
+    assert not any(key.endswith(".Title") for key in entries)
+    assert "Hello" in entries.values()
+
+
 def test_xaml_binding_is_not_localized_and_conflicting_namespace_fails():
     src = '<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"><Button Content="{x:Bind ViewModel.Label}"/></Window>'
     _, entries = transform_xaml(src, "A.xaml")
